a2c1c1faacc9a75b529d9f91befd14f7b8fd4c31
Sebastien Lemetter <sebaleme@gmail.com>
2021-02-16 22:23:48 +0100

    Feature: now example 1 is working for any amonth of spaces in the string. However, works only for a 2 term operation

--- infrastructure/exercices_diverse/src/regex_search.cpp
+++ infrastructure/exercices_diverse/src/regex_search.cpp
@@ -18,31 +18,31 @@ public:
     {
 
         int result = 0;
-        regex reg("(\\d+)\\s(\\+|\\-)\\s(\\d+)");
+        regex reg("(\\s+?)(\\d+)(\\s+?)(\\+|\\-)(\\s+?)(\\d+)(\\s+?)");
         smatch match;
 
         std::regex_search(s, match, reg);
         cout << "match.length(): " << match.length() << endl;
-        if((match.length() > 0U)&&(match[2].str()== "+"))
+        if((match.length() > 0U)&&(match[4].str()== "+"))
         {
-            result = stoi(match[1].str()) + stoi(match[3].str());
+            result = stoi(match[2].str()) + stoi(match[6].str());
         }
-        else if((match.length() > 0U)&&(match[2].str()== "-"))
+        else if((match.length() > 0U)&&(match[4].str()== "-"))
         {
-            result = stoi(match[1].str()) - stoi(match[3].str());            
+            result = stoi(match[2].str()) - stoi(match[6].str());            
         }
-        // for (std::size_t i = 0; i < match.size(); ++i)
-        // {
-        //     std::string num = match[i].str();
-        //     cout << i << ": " << num << endl;
-        // }
+        for (std::size_t i = 0; i < match.size(); ++i)
+        {
+            std::string num = match[i].str();
+            cout << i << ": " << num << endl;
+        }
         return result;
     }
 };
 
 int main()
 {
-    string s = "10 - 8";
+    string s = "   200 -   80  ";
     Solution soluce;
     int result = soluce.calculate(s);
     cout << endl;    
