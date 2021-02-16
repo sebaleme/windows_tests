de3b93c96fb7c54700b865941aeb25b483817b71
Sebastien Lemetter <sebaleme@gmail.com>
2021-02-16 23:45:09 +0100

    Feature: regex search, now example 2 is also working, along with constraint 1

--- infrastructure/exercices_diverse/src/regex_search.cpp
+++ infrastructure/exercices_diverse/src/regex_search.cpp
@@ -6,8 +6,13 @@
 # ****************************************************************************/
 
 // https://leetcode.com/problems/basic-calculator/description/
+
+// Soluce inspired from:
+// http://www.enseignement.polytechnique.fr/informatique/INF478/docs/Cpp/en/cpp/regex/regex_iterator.html
+
 #include <regex>
 #include <iostream>
+#include <map>
 
 using namespace std;
 
@@ -16,35 +21,65 @@ class Solution
 public:
     int calculate(string s)
     {
-
         int result = 0;
-        regex reg("(\\s+?)(\\d+)(\\s+?)(\\+|\\-)(\\s+?)(\\d+)(\\s+?)");
-        smatch match;
 
-        std::regex_search(s, match, reg);
-        cout << "match.length(): " << match.length() << endl;
-        if((match.length() > 0U)&&(match[4].str()== "+"))
+        // Stage 1: extract operators from string
+        std::map<int, string> operatorMap;
+        regex reg1("(\\+|\\-)");
+        auto words_begin = std::sregex_iterator(s.begin(), s.end(), reg1);
+        auto words_end = std::sregex_iterator();
+
+        std::ptrdiff_t const operatorNumber(std::distance(words_begin, words_end));
+        cout << " (number of operators): " << operatorNumber << endl;
+
+        int count = 0;
+
+        for (std::sregex_iterator i = words_begin; i != words_end; ++i)
         {
-            result = stoi(match[2].str()) + stoi(match[6].str());
+            std::smatch match = *i;
+            std::string match_str = match.str();
+            operatorMap.insert(pair<int, string>(count, match_str));
+            count++;
         }
-        else if((match.length() > 0U)&&(match[4].str()== "-"))
+
+        // Stage 2: extract operand from string and compute operation
+        regex reg2("[0-9]+");
+        auto words_begin2 = std::sregex_iterator(s.begin(), s.end(), reg2);
+        auto words_end2 = std::sregex_iterator();
+
+        int count2 = 0;
+
+        for (std::sregex_iterator i = words_begin2; i != words_end2; ++i)
         {
-            result = stoi(match[2].str()) - stoi(match[6].str());            
+            std::smatch match2 = *i;
+            std::string match_str2 = match2.str();
+
+            if (count2 == 0)
+            {
+                result = stoi(match_str2);
+            }
+            else
+            {
+                if (operatorMap[count2 - 1] == "+")
+                {
+                    result = result + stoi(match_str2);
+                }
+                else if (operatorMap[count2 - 1] == "-")
+                {
+                    result = result - stoi(match_str2);
+                }
+            }
+            count2++;
         }
-        for (std::size_t i = 0; i < match.size(); ++i)
-        {
-            std::string num = match[i].str();
-            cout << i << ": " << num << endl;
-        }
+
         return result;
     }
 };
 
 int main()
 {
-    string s = "   200 -   80  ";
+    string s = "1+   20000 -   8000 + 24-100 ";
     Solution soluce;
     int result = soluce.calculate(s);
-    cout << endl;    
     cout << result << endl;
 }
