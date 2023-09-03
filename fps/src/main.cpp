e43a38fffd5c09515856b351618a0f8ebb4e31d3
Sebastien Lemetter (CC-DA/EAR1) <sebastien.lemetter@de.bosch.com>
2023-09-03 17:14:53 +0200

    Feature: implement initial hello world and check that SDL lib is properly configured

---
+++ fps/src/main.cpp
@@ -0,0 +1,15 @@
+/* *****************************************************************************
+##
+# @file        main.cpp
+# @brief       main method for the FPS game. This app implements basic games functionalities
+# @copyright   MIT license
+# ****************************************************************************/
+
+#include <iostream>
+#include <SDL3/SDL.h>
+
+int main()
+{
+    std::cout << "Hello world" << std::endl;
+    return 0;
+}
