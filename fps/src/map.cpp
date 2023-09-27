975cb9615181ce607d4efee129c817058485f4b9
sebaleme <sebaleme@gmail.com>
2023-09-27 19:58:10 +0200

    Feature: check input from computeCellIndex are within bounds

--- fps/src/map.cpp
+++ fps/src/map.cpp
@@ -5,12 +5,14 @@
 # @copyright   MIT license
 # ****************************************************************************/
 #include "map.hpp"
-
+#include <cassert>
 #include "types.hpp"
 
 // The pixels array starts from bottom left, whereas the map definition starts from top left
 int32_t computeCellIndex(int32_t cell_row, int32_t cell_column)
 {
+    assert(cell_row < MAP_HEIGHT && "input from computeCellIndex out of range");
+    assert(cell_column < MAP_WIDTH && "input from computeCellIndex out of range");
     return (MAP_HEIGHT - 1 - cell_row)*MAP_WIDTH + cell_column;
 }
 
