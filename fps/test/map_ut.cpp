d23261f5562e6a2980df4ae12e308ec8935cbdd6
sebaleme <sebaleme@gmail.com>
2023-09-27 19:50:24 +0200

    Feature: implement UT for computeCellIndex function

--- fps/test/map_ut.cpp
+++ fps/test/map_ut.cpp
@@ -9,10 +9,41 @@
 
 #include <gtest/gtest.h>
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+// Check that the map access is working, considering that the cell coordinates
+// are given in the classical coordonate system, (0,0) is bottom left
+
+// Check the origin cell (positionBottomLeft)
+TEST(computeCellIndex, cellIndex_test1) {
+  // cell_row is going up, and cell_column is increasing to the right
+  int32_t cell_row{0}, cell_column{0};
+  int32_t cellIndex = computeCellIndex(cell_row, cell_column);
+  EXPECT_EQ(cellIndex, MAP_WIDTH*(MAP_HEIGHT-1));
+}
+
+// Check the cell (1,1), the first which is not a wall (initialPlayerPosition)
+TEST(computeCellIndex, cellIndex_test2) {
+  int32_t cell_row{1}, cell_column{1};
+  int32_t cellIndex = computeCellIndex(cell_row, cell_column);
+  EXPECT_EQ(cellIndex, MAP_WIDTH*(MAP_HEIGHT-2)+1);
+}
+
+// Check the cell (0,MAP_WIDTH-1), the bottom right corner
+TEST(computeCellIndex, cellIndex_test3) {
+  int32_t cell_row{0}, cell_column{MAP_WIDTH-1};
+  int32_t cellIndex = computeCellIndex(cell_row, cell_column);
+  EXPECT_EQ(cellIndex, MAP_WIDTH*MAP_HEIGHT-1);
+}
+
+// Check the cell (MAP_HEIGHT-1,0), the top left corner
+TEST(computeCellIndex, cellIndex_test4) {
+  int32_t cell_row{MAP_HEIGHT-1}, cell_column{0};
+  int32_t cellIndex = computeCellIndex(cell_row, cell_column);
+  EXPECT_EQ(cellIndex, 0);
+}
+
+// Check the cell (3,5), a random cell
+TEST(computeCellIndex, cellIndex_test5) {
+  int32_t cell_row{3}, cell_column{5};
+  int32_t cellIndex = computeCellIndex(cell_row, cell_column);
+  EXPECT_EQ(cellIndex, MAP_WIDTH*4+cell_column);
 }
