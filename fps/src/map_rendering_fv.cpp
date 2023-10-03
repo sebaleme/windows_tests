4b4552e84b53d6a52b8f589f7caa3b08c371aabc
sebaleme <sebaleme@gmail.com>
2023-10-03 16:21:21 +0200

    Feature: fix further distance bugs

--- fps/src/map_rendering_fv.cpp
+++ fps/src/map_rendering_fv.cpp
@@ -91,10 +91,13 @@ void construct_world(uint32_t* pixels, const StatePlayer& f_player)
                     // Case north cell is the next
                     ++cell_row;
                     intersecX += cosfAngle*distanceNorthCell;
+                    // Going up, so intersection is at the bottom of the upward cell
                     intersecY = static_cast<float_t>(cell_row * CELL_SIZE_PIXELS);
                     intersectionSide = true;
-                    // intersecX is the X coordinate of the intersection with the obstacle, so we
-                    // need to substract the player x position to compute the ray length
+                    // "intersecX" is the X coordinate of the intersection with the obstacle, so we
+                    // need to substract the player x position to compute the ray length.
+                    // "distanceNorthCell" is just the current distance within the current cell, 
+                    // and not from the player position
                     distanceToObstacle = (intersecX - f_player.x) / cosfAngle;
                 }
                 else
@@ -139,17 +142,17 @@ void construct_world(uint32_t* pixels, const StatePlayer& f_player)
                 {
                     // Case west cell is the next
                     cell_col--;
-                    intersecX = static_cast<float_t>(cell_col * CELL_SIZE_PIXELS);;
+                    intersecX = static_cast<float_t>((cell_col + 1) * CELL_SIZE_PIXELS);
                     intersecY += sinfAngle*distanceWestCell;
                     distanceToObstacle = (intersecY - f_player.y) / sinfAngle;
                 }
                 cellIndex = computeCellIndex(cell_row,cell_col);
-                if (distanceToObstacle > static_cast<float_t>(SCREEN_WIDTH) * sqrt(2.F))
-                {
-                    std::cout << "unplausible angle" << std::endl;
-                }
-                fillColumn(pixels, pixel_col, cellIndex, distanceToObstacle, intersectionSide);
             }
+            if (distanceToObstacle > static_cast<float_t>(SCREEN_WIDTH) * sqrt(2.F))
+            {
+                std::cout << "unplausible angle" << std::endl;
+            }
+            fillColumn(pixels, pixel_col, cellIndex, distanceToObstacle, intersectionSide);
         }
         else if((angleRad > -(SDL_PI_F / 2.F)) && (angleRad < 0.F))
         {
@@ -168,7 +171,8 @@ void construct_world(uint32_t* pixels, const StatePlayer& f_player)
                     // Case south cell is the next
                     cell_row--;
                     intersecX += cosfAngle * distanceSouthCell;
-                    intersecY = static_cast<float_t>(cell_row * CELL_SIZE_PIXELS);
+                    // Going down, so intersection is at the top of the downward cell, hence +1
+                    intersecY = static_cast<float_t>((cell_row+1) * CELL_SIZE_PIXELS);
                     distanceToObstacle = (intersecX - f_player.x) / cosfAngle;
                 }
                 else
@@ -180,12 +184,12 @@ void construct_world(uint32_t* pixels, const StatePlayer& f_player)
                     distanceToObstacle = (intersecY - f_player.y) / sinfAngle;
                 }
                 cellIndex = computeCellIndex(cell_row,cell_col);
-                if (distanceToObstacle > static_cast<float_t>(SCREEN_WIDTH) * sqrt(2.F))
-                {
-                    std::cout << "unplausible angle" << std::endl;
-                }
-                fillColumn(pixels, pixel_col, cellIndex, distanceToObstacle, intersectionSide);
             }
+            if (distanceToObstacle > static_cast<float_t>(SCREEN_WIDTH) * sqrt(2.F))
+            {
+                std::cout << "unplausible angle" << std::endl;
+            }
+            fillColumn(pixels, pixel_col, cellIndex, distanceToObstacle, intersectionSide);
         }
         else if ((angleRad > -SDL_PI_F) && (angleRad < -SDL_PI_F/2))
         {
@@ -204,7 +208,8 @@ void construct_world(uint32_t* pixels, const StatePlayer& f_player)
                     // Case south cell is the next
                     cell_row--;
                     intersecX += cosfAngle * distanceSouthCell;
-                    intersecY = static_cast<float_t>(cell_row * CELL_SIZE_PIXELS);
+                    // Going down, so intersection is at the top of the downward cell, hence +1
+                    intersecY = static_cast<float_t>((cell_row + 1) * CELL_SIZE_PIXELS);
                     // Cos is negative, but f_player.x is greater than intersectX hence positive distance
                     distanceToObstacle = (intersecX - f_player.x) / cosfAngle;
                 }
@@ -212,32 +217,21 @@ void construct_world(uint32_t* pixels, const StatePlayer& f_player)
                 {
                     // Case west cell is the next
                     cell_col--;
-                    intersecX = static_cast<float_t>(cell_col * CELL_SIZE_PIXELS);;
+                    // New position is east of the west cell, so intersecX is cell_col+1
+                    intersecX = static_cast<float_t>((cell_col+1) * CELL_SIZE_PIXELS);
                     intersecY += sinfAngle * distanceWestCell;
                     // Sin is negative, but f_player.y is greater than intersectX, so we have a positive distance
                     distanceToObstacle = (intersecY - f_player.y) / sinfAngle;
                 }
                 cellIndex = computeCellIndex(cell_row, cell_col);
-                if (distanceToObstacle > static_cast<float_t>(SCREEN_WIDTH) * sqrt(2.F))
-                {
-                    std::cout << "unplausible angle" << std::endl;
-                }
-                fillColumn(pixels, pixel_col, cellIndex, distanceToObstacle, intersectionSide);
             }
-        }
-        else if( angleRad == 0.F)
-        {
-            // Look for the first obstacle
-            while(g_map[cellIndex] == 0)
+            if (distanceToObstacle > static_cast<float_t>(SCREEN_WIDTH) * sqrt(2.F))
             {
-                cell_col++;
-                cellIndex = computeCellIndex(cell_row,cell_col);
+                std::cout << "unplausible angle" << std::endl;
             }
-            int32_t player_col = static_cast<int32_t>(f_player.x) / CELL_SIZE_PIXELS;
-            distanceToObstacle = cell_col* CELL_SIZE_PIXELS - f_player.x;
-            fillColumn(pixels, pixel_col, cellIndex, distanceToObstacle, false);
+            fillColumn(pixels, pixel_col, cellIndex, distanceToObstacle, intersectionSide);
         }
-        else if (angleRad == SDL_PI_F / 2.F)
+        else if ((angleRad == -SDL_PI_F / 2.F) || (angleRad == SDL_PI_F / 2.F))
         {
             // Look for the first obstacle
             while (g_map[cellIndex] == 0)
@@ -245,31 +239,15 @@ void construct_world(uint32_t* pixels, const StatePlayer& f_player)
                 cell_row = (angleRad == SDL_PI_F / 2.F) ? cell_row + 1 : cell_row - 1;
                 cellIndex = computeCellIndex(cell_row, cell_col);
             }
-            int32_t player_col = static_cast<int32_t>(f_player.x) / CELL_SIZE_PIXELS;
-            distanceToObstacle = abs(cell_col - player_col) * CELL_SIZE_PIXELS + f_player.x - player_col * CELL_SIZE_PIXELS;
+            int32_t player_row = static_cast<int32_t>(f_player.y) / CELL_SIZE_PIXELS;
+            distanceToObstacle = abs(cell_row - player_row) * CELL_SIZE_PIXELS + f_player.y - player_row * CELL_SIZE_PIXELS;
             if (distanceToObstacle > static_cast<float_t>(SCREEN_WIDTH) * sqrt(2.F))
             {
                 std::cout << "unplausible angle" << std::endl;
             }
             fillColumn(pixels, pixel_col, cellIndex, distanceToObstacle, false);
         }
-        else if (angleRad == -SDL_PI_F / 2.F)
-        {
-            // Look for the first obstacle
-            while (g_map[cellIndex] == 0)
-            {
-                cell_row = cell_row - 1;
-                cellIndex = computeCellIndex(cell_row, cell_col);
-            }
-            int32_t player_col = static_cast<int32_t>(f_player.x) / CELL_SIZE_PIXELS;
-            distanceToObstacle = abs(cell_col - player_col) * CELL_SIZE_PIXELS + f_player.x - player_col * CELL_SIZE_PIXELS;
-            if (distanceToObstacle > static_cast<float_t>(SCREEN_WIDTH) * sqrt(2.F))
-            {
-                std::cout << "unplausible angle" << std::endl;
-            }
-            fillColumn(pixels, pixel_col, cellIndex, distanceToObstacle, false);
-        }
-        else if((angleRad == SDL_PI_F)|| (angleRad == -SDL_PI_F))
+        else if((angleRad == SDL_PI_F) || (angleRad == -SDL_PI_F) ||  (angleRad == 0.F))
         {
             // Look for the first obstacle
             while(g_map[cellIndex] == 0)
@@ -277,22 +255,8 @@ void construct_world(uint32_t* pixels, const StatePlayer& f_player)
                 cell_col = (angleRad == 0.F) ? cell_col + 1 : cell_col - 1;
                 cellIndex = computeCellIndex(cell_row,cell_col);
             }
-            distanceToObstacle = intersecX / cosfAngle;
-            if (distanceToObstacle > static_cast<float_t>(SCREEN_WIDTH) * sqrt(2.F))
-            {
-                std::cout << "unplausible angle" << std::endl;
-            }
-            fillColumn(pixels, pixel_col, cellIndex, distanceToObstacle, false);
-        }
-        else if (angleRad == 0.F)
-        {
-            // Look for the first obstacle
-            while (g_map[cellIndex] == 0)
-            {
-                cell_col = (angleRad == 0.F) ? cell_col + 1 : cell_col - 1;
-                cellIndex = computeCellIndex(cell_row, cell_col);
-            }
-            distanceToObstacle = intersecX / cosfAngle;
+            int32_t player_col = static_cast<int32_t>(f_player.x) / CELL_SIZE_PIXELS;
+            distanceToObstacle = abs(cell_col - player_col) * CELL_SIZE_PIXELS + f_player.x - player_col * CELL_SIZE_PIXELS;
             if (distanceToObstacle > static_cast<float_t>(SCREEN_WIDTH) * sqrt(2.F))
             {
                 std::cout << "unplausible angle" << std::endl;
